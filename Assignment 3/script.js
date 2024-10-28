6c2c159ecbedbe6b2f27c79dbdd65a7151537442
Kelly Ga Yi Xiao <s4002452@student.rmit.edu.au>
2024-10-28 16:04:02 +1100

    final changes

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,3 +1,10 @@
+window.addEventListener("load", function () {
+  const audio = document.getElementById("background-music");
+  audio.play().catch((error) => {
+    console.log("Autoplay was blocked. Waiting for user interaction.");
+  });
+}); //Trying to get the music to autoplay when my website loads, but it still doesn't work :(
+
 function changeAvatar(outfitImage) {
   document.getElementById("avatar").src = outfitImage; // Changes the avatar image
 }
