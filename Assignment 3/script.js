982058ff80cba894e76edba2f8ad8c5391269fec
Kelly Ga Yi Xiao <s4002452@student.rmit.edu.au>
2024-10-28 11:28:41 +1100

    added end game page

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -1,3 +1,8 @@
 function changeAvatar(outfitImage) {
   document.getElementById("avatar").src = outfitImage; // Changes the avatar image
 }
+
+function confirmSelection() {
+  // Redirect to the confirmation page
+  window.location.href = "endgame.html"; // Change this to your target page
+}
