91a7e6d49ad3355e212d43ffcb3487fa5f55304e
Kelly Ga Yi Xiao <s4002452@student.rmit.edu.au>
2025-05-20 23:37:43 +1000

    no. 2 sound

--- dms4/assignment3/toneSetup.js
+++ dms4/assignment3/toneSetup.js
@@ -1,32 +1,50 @@
-const synth = new Tone.MonoSynth().toDestination();
+const distortion = new Tone.Distortion(0.6).toDestination();
+
+const synth = new Tone.Synth({
+  oscillator: {
+    type: "square",
+  },
+  filter: {
+    Q: 1,
+    type: "lowpass",
+    rolloff: -12,
+  },
+  envelope: {
+    attack: 0.01,
+    decay: 0,
+    sustain: 0.2,
+    release: 0.2,
+  },
+}).connect(distortion);
+
 const typingBox = document.getElementById("typingBox");
 
 const letterMap = {
-  a: "c3",
-  b: "d3",
-  c: "e3",
-  d: "f3",
-  e:,
-  f:,
-  g:,
-  h:,
-  i:,
-  j:,
-  k:,
-  l:,
-  m:,
-  o:,
-  p:,
-  q:,
-  r:,
-  s:,
-  t:,
-  u:,
-  v:,
-  w:,
-  x:,
-  y:,
-  z:,
+  a: "c2",
+  b: "d2",
+  c: "e2",
+  d: "a2",
+  e: "b2",
+  f: "c2",
+  g: "d2",
+  h: "e2",
+  i: "a2",
+  j: "b2",
+  k: "c2",
+  l: "d2",
+  m: "e2",
+  o: "d2",
+  p: "b2",
+  q: "c2",
+  r: "d2",
+  s: "e2",
+  t: "a2",
+  u: "e2",
+  v: "c2",
+  w: "d2",
+  x: "e2",
+  y: "a2",
+  z: "b2",
 };
 
 async function playSound(event) {
