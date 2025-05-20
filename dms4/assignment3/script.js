8c25ea9d29b506bc0743aff2e93f442e568070fe
Kelly Ga Yi Xiao <s4002452@student.rmit.edu.au>
2025-05-20 21:43:07 +1000

    narrative script changes

--- dms4/assignment3/script.js
+++ dms4/assignment3/script.js
@@ -1,8 +1,15 @@
 const questions = [
   "Who are you?",
-  "What is your name?",
-  "Question 2",
-  "Question 3",
+  "What is your name, Earthling?",
+  "Nevermind, human names are bothersome. I'll just call you Earthling.",
+  "Earthling, we are compiling intelligence on Earth lifeforms. Can you describe how your day went?",
+  "Ah, yes — classic Earthling behavior. Emotional turbulence, minor victories, and caffeine-based sustenance. Interesting.  ",
+  "If one day, an apocalyse came and you're free from work, what would you do with your freedom? Hypothetically, of course :)",
+  "Intruiging. Without work to define your existence, who are you as a person?",
+  "I see. Thank you for your input, Earthling. You're free to do whatever you like with your remaining time on earth.",
+  "...",
+  "...",
+  "Persistent Earthing, do you want to die faster with what short time you have left ^^?",
 ];
 
 let currentQuestion = 0;
